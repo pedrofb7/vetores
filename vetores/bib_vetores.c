79ccf3c8097881ba9914f4fbb5453932c28e09fe
Pedro Ferreira de Brito <tablet.pfb@gmail.com>
2025-04-25 20:28:00 -0300

    Update bib_vetores.c

--- vetores/bib_vetores.c
+++ vetores/bib_vetores.c
@@ -3,14 +3,15 @@
 #include <time.h>
 #include "bib_vetores.h"
 
-void gerar_valor_vet(int tamanho, int vetor[], int valor_limite) {
+// 
+void gerar_valor_vet_int(int tamanho, int vetor[], int valor_minimo, int valor_limite) {
 
     srand(time(NULL));
 
 
  for(int i = 0; i < tamanho; tamanho ++) {
 
-        int aleatório = 1 + rand() % valor_limite;
+        int aleatório = valor_minimo + rand() % (valor_limite - valor_minimo);
 
         vetor[tamanho] = aleatório;
         
@@ -20,9 +21,39 @@ void gerar_valor_vet(int tamanho, int vetor[], int valor_limite) {
 
 }
 
+int aleatorio_inteiro(int max, int min)
+{
+
+    int num_aleatorio;
+
+    num_aleatorio = min + rand() % max;
+
+    return num_aleatorio;
+}
+
+void preencher_vetor_inteiro(int tamanho, int vetor[], int valor_inteiro)
+{
+
+    for (int i = 0; i < tamanho; tamanho++)
+    {
+
+        vetor[tamanho] = valor_inteiro;
+    }
+}
+
+void exibir_vetor_inteiro(int tamanho, int vetor[]) { 
+
+    for (int i = 0; i < tamanho; i++) {
+
+        printf("%d", vetor[i]);
+
+    }
+
+}
+
 void multiplicar_vetores(int tamanho, int a[], int b[]) {
 
-    for (tamanho = 0; tamanho )
+    
 
 
 } 
